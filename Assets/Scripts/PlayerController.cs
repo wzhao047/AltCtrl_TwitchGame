bf7fe63e5186e499866d6b74d145e03c33df5382
wzhao4 <zhaoweiting047@gmail.com>
2025-10-22 19:43:41 -0700

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float moveSpeed = 5f;      
+    public float moveLimitY = 4f;     
+
     void Update()
     {
+        float moveDir = 0f;
+
+
+         if (Keyboard.current != null)
+        {
+            if (Keyboard.current.wKey.isPressed) moveDir = 1f;
+            else if (Keyboard.current.sKey.isPressed) moveDir = -1f;
+        }
+
         
+
+        Vector3 newPos = transform.position + Vector3.up * moveDir * moveSpeed * Time.deltaTime;
+
+
+        newPos.y = Mathf.Clamp(newPos.y, -moveLimitY, moveLimitY);
+
+
+        transform.position = newPos;
     }
 }
